39c715b7b802cae88d93e2075dc3ee25705477e2
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2021-10-31 12:43:26 +0000

    Add scoreboard component to display scores

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSquishmallows from '../../hooks/useSquishmallows';
 import classes from './Game.module.css';
 import CardGrid from './CardGrid';
+import Scoreboard from './Scoreboard';
 
 export default function Game() {
     const { activeSquishmallows, shuffleDeck } = useSquishmallows(4);
     return (
         <main className={classes.container}>
+            <Scoreboard currentScore="0" bestScore="0" />
             <CardGrid
                 squishmallows={activeSquishmallows}
                 shuffleDeck={shuffleDeck}
---
+++ src/components/Game/Scoreboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Scoreboard.module.css';
+
+export default function Scoreboard({ currentScore, bestScore }) {
+    return (
+        <div className={classes.container}>
+            <p className={`${classes.score} ${classes.currentScore}`}>
+                Current Score: {currentScore}
+            </p>
+            <p className={`${classes.score} ${classes.bestScore}`}>
+                Best Score: {bestScore}
+            </p>
+        </div>
+    );
+}
